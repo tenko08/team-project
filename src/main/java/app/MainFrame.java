68d570355f56f8619fac003bd512489f74068f58
PixelatedMike <michaeldk218@gmail.com>
2025-11-10 13:59:42 -0500

    mainframe

---
+++ src/main/java/app/MainFrame.java
@@ -0,0 +1,16 @@
+package app;
+
+import javax.swing.*;
+
+public class MainFrame {
+    private static final String TITLE = "Real-Time TTC Map Viewer";
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame(TITLE);
+            frame.setMinimumSize(new java.awt.Dimension(300, 200));
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
