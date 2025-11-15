c02a62c9e4bef47d4827ab19745cd9d99d31cbc9
UTSAMA Light Music Club <utsama.lightmusicclub@gmail.com>
2025-11-15 04:07:54 -0500

    basic display of map of Toronto

--- src/main/java/app/MainFrame.java
+++ src/main/java/app/MainFrame.java
@@ -1,15 +1,40 @@
 package app;
 
-import entities.*;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-import javax.swing.*;
-
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
 
 public class MainFrame {
     private static final String TITLE = "Real-Time TTC Map Viewer";
+    private static final double LAT = 43.65;
+    private static final double LON = -79.38;
+
     public static void main(String[] args) {
+        JXMapViewer mapViewer = new JXMapViewer();
+
+        // Create a TileFactoryInfo for OpenStreetMap
+        TileFactoryInfo info = new OSMTileFactoryInfo("OpenStreetMap", "https://tile.openstreetmap.org");
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        tileFactory.setUserAgent("TTC Map Viewer/1.0 (contact: michaeld.kim@mail.utoronto.ca)");
+        mapViewer.setTileFactory(tileFactory);
+
+        // Use 8 threads in parallel to load the tiles
+        tileFactory.setThreadPoolSize(8);
+
+        // Set the focus
+        GeoPosition toronto = new GeoPosition(LAT, LON);
+
+        mapViewer.setZoom(7);
+        mapViewer.setAddressLocation(toronto);
+
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame(TITLE);
+            frame.getContentPane().add(mapViewer);
             frame.setMinimumSize(new java.awt.Dimension(300, 200));
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
