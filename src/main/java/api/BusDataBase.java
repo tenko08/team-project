f54cec5b8d5ce1e32e9c74e29152a8cfcd65c069
liyich13 <yichenn.li@mail.utoronto.ca>
2025-11-17 14:28:06 -0500

    finished busapi

--- src/main/java/api/BusDataBase.java
+++ src/main/java/api/BusDataBase.java
@@ -2,10 +2,12 @@ package api;
 
 import entities.Bus;
 
+import java.util.List;
+
 public interface BusDataBase {
     Bus getBus(int id);
 
-    Bus[] getAllBuses();
+    List<Bus> getAllBuses();
 
 
 }
--- src/main/java/api/BusDataBaseAPI.java
+++ src/main/java/api/BusDataBaseAPI.java
@@ -3,6 +3,7 @@ package api;
 import entities.Bus;
 
 import com.google.transit.realtime.GtfsRealtime;
+import entities.Position;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,18 +15,20 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BusDataBaseAPI implements BusDataBase {
     private static final String API_URL = "https://bustime.ttc.ca/gtfsrt/vehicles";
 
-
-
     @Override
-    public Bus getBus(int id) {
+    public List<Bus> getAllBuses() {
         final OkHttpClient client = new OkHttpClient();
 
         final Request request = new Request.Builder().url(API_URL).build();
+
+        List<Bus> buses = new ArrayList<>();
 
         try {
             final Response response = client.newCall(request).execute();
@@ -34,32 +37,59 @@ public class BusDataBaseAPI implements BusDataBase {
             GtfsRealtime.FeedMessage feed =
                     GtfsRealtime.FeedMessage.parseFrom(bytes);
 
-            System.out.println(feed);
+//            System.out.println(feed);
 
             for (GtfsRealtime.FeedEntity entity : feed.getEntityList()) {
-                System.out.println(entity);
+//                System.out.println(entity);
+                int vehicleId = -1;
+                Position position = null;
+                String occupancy = "UNKNOWN";
+
                 if (entity.hasVehicle()) {
                     GtfsRealtime.VehiclePosition vp = entity.getVehicle();
 
                     if (vp.hasVehicle() && vp.getVehicle().hasId()) {
+                        vehicleId = Integer.parseInt(vp.getVehicle().getId());
+                    }
+
+                    if (vp.hasVehicle() && vp.hasPosition()) {
+                        position = new Position(vp.getPosition().getLatitude(), vp.getPosition().getLongitude(), vp.getPosition().getBearing(),
+                                vp.getPosition().getSpeed());
 
                     }
+
+                    if (vp.hasVehicle() && vp.hasOccupancyStatus()) {
+                        occupancy = String.valueOf(vp.getOccupancyStatus());
+                    }
+
                 }
+                Bus bus = new Bus(vehicleId, position, occupancy);
+                buses.add(bus);
             }
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new Bus(1);
+        System.out.println(buses);
+        return buses;
+
     }
 
     @Override
-    public Bus[] getAllBuses() {
-        return new Bus[0];
+    public Bus getBus(int id) {
+        List<Bus> buses = getAllBuses();
+
+        for (Bus bus : buses) {
+            if (bus.getId() == id) {
+                return bus;
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        new BusDataBaseAPI().getBus(5);
+        new BusDataBaseAPI().getAllBuses();
 
     }
 }
