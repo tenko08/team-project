f6cfca8677ba25b265d228e285a5687aede98c87
UTSAMA Light Music Club <utsama.lightmusicclub@gmail.com>
2025-11-17 02:28:42 -0500

    CA-worthy separation of map code

--- src/main/java/view/MapView.java
+++ src/main/java/view/MapView.java
@@ -1,4 +1,38 @@
 package view;
 
-public class MapView {
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
+import javax.swing.*;
+
+public class MapView extends JPanel {
+    private final String viewName = "map";
+    private static final double LAT = 43.65;
+    private static final double LON = -79.38;
+
+    public MapView() {
+        JXMapViewer mapViewer = new JXMapViewer();
+
+        // Create a TileFactoryInfo for OpenStreetMap
+        TileFactoryInfo info = new OSMTileFactoryInfo("OpenStreetMap", "https://tile.openstreetmap.org");
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        tileFactory.setUserAgent("TTC Map Viewer/1.0 (contact: michaeld.kim@mail.utoronto.ca)");
+        mapViewer.setTileFactory(tileFactory);
+
+        // Use 8 threads in parallel to load the tiles
+        tileFactory.setThreadPoolSize(8);
+
+        // Set the focus
+        GeoPosition toronto = new GeoPosition(LAT, LON);
+
+        mapViewer.setZoom(7);
+        mapViewer.setAddressLocation(toronto);
+
+        this.add(mapViewer);
+    }
+
+    public String getViewName() { return viewName; }
 }
