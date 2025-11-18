9632c3c4da94b9c9095e3391552ed2a7daffb933
BryanYeeee <haloday873@gmail.com>
2025-11-18 17:47:18 -0500

    presenter start

--- src/main/java/interface_adapter/find_nearest_route/FindNearestRoutePresenter.java
+++ src/main/java/interface_adapter/find_nearest_route/FindNearestRoutePresenter.java
@@ -1,4 +1,24 @@
 package interface_adapter.find_nearest_route;
 
-public class FindNearestRoutePresenter {
+import use_case.find_nearest_route.FindNearestRouteOutputBoundary;
+import use_case.find_nearest_route.FindNearestRouteOutputData;
+
+public class FindNearestRoutePresenter implements FindNearestRouteOutputBoundary {
+
+//    private final FindNearestRouteViewModel findNearestRouteViewModel;
+//    private final ViewManagerModel viewManagerModel;
+//
+//    public FindNearestRoutePresenter (ViewManagerModel viewManagerModel) {
+//        this.viewManagerModel = viewManagerModel;
+//    }
+
+    @Override
+    public void prepareSuccessView(FindNearestRouteOutputData outputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String message) {
+
+    }
 }
--- src/main/java/use_case/find_nearest_route/FindNearestRouteOutputBoundary.java
+++ src/main/java/use_case/find_nearest_route/FindNearestRouteOutputBoundary.java
@@ -1,6 +1,6 @@
 package use_case.find_nearest_route;
 
 public interface FindNearestRouteOutputBoundary {
-    void prepareFailView(String message);
     void prepareSuccessView(FindNearestRouteOutputData outputData);
+    void prepareFailView(String message);
 }
