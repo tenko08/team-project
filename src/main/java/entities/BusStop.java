155f4721a296ff30d10fdc51ae10dd3fc2599cfb
BryanYeeee <haloday873@gmail.com>
2025-11-17 23:06:20 -0500

    fix entities

--- src/main/java/entities/BusStop.java
+++ src/main/java/entities/BusStop.java
@@ -1,37 +1,25 @@
 package entities;
 
-import java.util.List;
-
 public class BusStop {
-    private String id;
+    private int id;
+    private int stopSequence;
     private String name;
-    private double latitude;
-    private double longitude;
-    private List<String> routes;
+    private Position position;
 
-    public BusStop(String id, String name, double latitude, double longitude, List<String> routes) {
+    public BusStop(int id, int stopSequence, String name, Position position) {
         this.id = id;
+        this.stopSequence = stopSequence;
         this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.routes = routes;
+        this.position = position;
     }
 
-    // Getters and setters
-    public String getId() {
+    // --- Getters ---
+    public int getId() {
         return id;
     }
-    public String getName() {
-        return name;
-    }
-    public double getLatitude() {
-        return latitude;
-    }
-    public double getLongitude() {
-        return longitude;
-    }
-    public List<String> getRoutes() {
-        return routes;
+
+    public int getStopSequence() {
+        return stopSequence;
     }
 
     public Position getPosition() {
