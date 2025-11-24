4f2a73baefd338074e4dadaf15131edee5910478
liyich13 <yichenn.li@mail.utoronto.ca>
2025-11-23 21:02:20 -0500

    updated outputboundary

--- src/main/java/use_case/search_by_route/SearchByRouteOutputBoundary.java
+++ src/main/java/use_case/search_by_route/SearchByRouteOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.search_by_route;
 
 public interface SearchByRouteOutputBoundary {
+    void prepareSuccessView(SearchByRouteOutputData outputData);
+    void prepareCachedView(SearchByRouteOutputData outputData);
+    void prepareFailView(String errorMessage);
 }
